85052b045bc4f5b424576a6b3e042f4271d1e1a1
shellynj <mb884@njit.edu>
2019-12-07 18:13:37 -0500

    Feature: Added Update a Blogposts - Put function added Part 2 Branch

--- src/views/BlogpostView.py
+++ src/views/BlogpostView.py
@@ -57,3 +57,25 @@ def custom_response(res, status_code):
     status=status_code
   )
 
+
+@blogpost_api.route('/<int:blogpost_id>', methods=['PUT'])
+@Auth.auth_required
+def update(blogpost_id):
+    """
+    Update A Blogpost
+    """
+    req_data = request.get_json()
+    post = BlogpostModel.get_one_blogpost(blogpost_id)
+    if not post:
+        return custom_response({'error': 'post not found'}, 404)
+    data = blogpost_schema.dump(post).data
+    if data.get('owner_id') != g.user.get('id'):
+        return custom_response({'error': 'permission denied'}, 400)
+
+    data, error = blogpost_schema.load(req_data, partial=True)
+    if error:
+        return custom_response(error, 400)
+    post.update(data)
+
+    data = blogpost_schema.dump(post).data
+    return custom_response(data, 200)
