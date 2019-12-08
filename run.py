5e8d19550a717bd50c1cf1008aad8ca64276951d
shellynj <mb884@njit.edu>
2019-12-07 19:53:34 -0500

    Task: Updated run.py file -  Part 3 Branch

--- run.py
+++ run.py
@@ -3,9 +3,10 @@ import os
 
 from src.app import create_app
 
-if __name__ == '__main__':
-  env_name = os.getenv('FLASK_ENV')
-  app = create_app(env_name)
-  # run app
-  app.run()
+env_name = os.getenv('FLASK_ENV')
+app = create_app(env_name)
 
+if __name__ == '__main__':
+  port = os.getenv('PORT')
+  # run app
+  app.run(host='0.0.0.0', port=port)
